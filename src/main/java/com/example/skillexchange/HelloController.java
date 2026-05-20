8a108e58a328d3f84faaa5a20da67ee090a7e51a
Sai Deepika Bhaskaruni <deepika.skillexchangeapp@gmail.com>
2026-05-21 00:03:13 +0530

    feat: add basic compilable spring boot structure

---
+++ src/main/java/com/example/skillexchange/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.skillexchange;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Skill Exchange API is running fine!";
+    }
+}
